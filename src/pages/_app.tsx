de1727dbd643a9e49affd213bef315ea88c20af1
yuichiyasui <yassan.b.a.w@gmail.com>
2021-11-27 12:11:21 +0900

    feat(*): 共通のheadタグを定義

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
 import 'modern-css-reset/dist/reset.min.css';
 import 'src/styles/globals.scss';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title key="title">Profile Book</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
