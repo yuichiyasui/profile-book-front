ed8b85d0578e017321ac9de3db53ba06fae131b7
yuichiyasui <yassan.b.a.w@gmail.com>
2021-11-29 01:50:34 +0900

    feat(login): ログイン画面を作成

---
+++ src/components/page/login/Login.tsx
@@ -0,0 +1,45 @@
+import Head from 'next/head';
+
+import styles from './Login.module.scss';
+
+import { Header } from 'src/components/ui/Header';
+import { FormEvent } from 'react';
+
+export const Login = () => {
+  const login = (e: FormEvent) => {
+    e.preventDefault();
+    alert('ログインしました。');
+  };
+
+  return (
+    <>
+      <Head>
+        <title key="title">ログイン | Profile Book</title>
+      </Head>
+
+      <Header />
+      <main>
+        <div className={styles.container}>
+          <section className={styles.login}>
+            <h1>ログイン</h1>
+            <form onSubmit={login}>
+              <div>
+                <label>
+                  <p>メールアドレス</p>
+                  <input type="email" placeholder="メールアドレス" />
+                </label>
+              </div>
+              <div>
+                <label>
+                  <p>パスワード</p>
+                  <input type="password" placeholder="パスワード" />
+                </label>
+              </div>
+              <button>ログイン</button>
+            </form>
+          </section>
+        </div>
+      </main>
+    </>
+  );
+};
