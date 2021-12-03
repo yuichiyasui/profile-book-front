2b58c1f1736f9a3518942053fece962c811de3bc
yuichiyasui <yassan.b.a.w@gmail.com>
2021-12-03 23:56:51 +0900

    feat(user-registration): フォームを作成

--- src/components/page/user-registration/UserRegistration.tsx
+++ src/components/page/user-registration/UserRegistration.tsx
@@ -1,8 +1,17 @@
+import { FormEvent } from 'react';
 import Head from 'next/head';
+import Link from 'next/Link';
+
+import styles from './UserRegistration.module.scss';
 
 import { Header } from 'src/components/ui/Header';
 
 export const UserRegistration = () => {
+  const register = (e: FormEvent) => {
+    e.preventDefault();
+    alert('ユーザー登録しました。');
+  };
+
   return (
     <>
       <Head>
@@ -11,7 +20,45 @@ export const UserRegistration = () => {
 
       <Header />
       <main>
-        <h1>ユーザー登録</h1>
+        <div className={styles.container}>
+          <section className={styles['user-registration']}>
+            <form onSubmit={register}>
+              <h1>ユーザー登録</h1>
+              <div>
+                <label>
+                  <p>メールアドレス</p>
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="メールアドレスを入力"
+                  />
+                </label>
+              </div>
+              <div>
+                <label>
+                  <p>パスワード</p>
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="パスワードを入力"
+                  />
+                </label>
+              </div>
+              <div>
+                <label>
+                  <p>パスワード再確認</p>
+                  <input
+                    type="password"
+                    name="confirm-password"
+                    placeholder="再度パスワードを入力"
+                  />
+                </label>
+              </div>
+              <button type="submit">登録</button>
+            </form>
+            <Link href="/login">ログイン画面に戻る</Link>
+          </section>
+        </div>
       </main>
     </>
   );
